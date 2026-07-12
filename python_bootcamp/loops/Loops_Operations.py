115b9194e88be21c45df471deb55334e4f60132c
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2026-07-12 21:15:41 +0530

    loops

--- python_bootcamp/loops/Loops_Operations.py
+++ python_bootcamp/loops/Loops_Operations.py
@@ -17,10 +17,18 @@ def range_operations():
     for i in range(0,10,2):
         print(i)
 
+def list_comprehension():
+    rectangles = [(2,3),(4,5),(6,7)]
+    rectangles_area = [(x,y,x*y) for (x,y) in rectangles]
+    print(rectangles)
+    print(rectangles_area)
+
+
 def main():
     while_loop()
     for_loop()
     range_operations()
+    list_comprehension()
 
 if __name__ == '__main__':
   main()
