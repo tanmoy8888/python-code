c6d623fedc83518f26dfb1ebf54f255a4eedb154
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2026-07-12 21:08:35 +0530

    loops

--- python_bootcamp/loops/Loops_Operations.py
+++ python_bootcamp/loops/Loops_Operations.py
@@ -12,9 +12,15 @@ def for_loop():
     for name in names:
         print(name)
 
+def range_operations():
+    print("Printing range elements")
+    for i in range(0,10,2):
+        print(i)
+
 def main():
     while_loop()
     for_loop()
+    range_operations()
 
 if __name__ == '__main__':
   main()
