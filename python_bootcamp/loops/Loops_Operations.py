30999bca7fdf431e7b3d698048f61614c849d244
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2026-07-12 21:05:25 +0530

    loops

--- python_bootcamp/loops/Loops_Operations.py
+++ python_bootcamp/loops/Loops_Operations.py
@@ -1,10 +1,20 @@
 
+def while_loop():
+    print("Printing while loops elements")
+    a = 0
+    while a < 5:
+        print(a)
+        a += 1
+
+def for_loop():
+    print("Printing for loops elements")
+    names = ["Tanmoy","Rahul","Raj","Rohit"]
+    for name in names:
+        print(name)
 
 def main():
-  a = 0
-  while a < 5:
-      print(a)
-      a +=1
+    while_loop()
+    for_loop()
 
 if __name__ == '__main__':
   main()
