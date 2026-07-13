e371f66e42ec218d612099d6f8ab661e8d88002d
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2026-07-13 20:32:33 +0530

    functions

--- python_bootcamp/functions/Functions_Operations.py
+++ python_bootcamp/functions/Functions_Operations.py
@@ -5,9 +5,13 @@ def even_odd(a):
     else:
        print("Odd")
 
+def mandatory_optional(first ="Hello", second="World"):
+    return first + " "+second
+
 def main():
     even_odd(5)
     print(even_odd(5))
+    print(mandatory_optional())
 
 if __name__ == '__main__':
    main()
