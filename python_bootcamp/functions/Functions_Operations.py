d0ee0585bcb1c6bd73551f28bdf957eae852b8a2
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2026-07-13 20:58:49 +0530

    functions

--- python_bootcamp/functions/Functions_Operations.py
+++ python_bootcamp/functions/Functions_Operations.py
@@ -8,12 +8,20 @@ def even_odd(a):
 def mandatory_optional(first ="Hello", second="World"):
     return first + " "+second
 
+def lambda_functions():
+    l = [1,2,3,4,5]
+    print(l)
+    square_list = map(lambda x : x*x,l)
+    print(list(square_list))
+
+
 def main():
     even_odd(5)
     print(even_odd(5))
     print(mandatory_optional())
     print(mandatory_optional("Hii"))
     print(mandatory_optional(second="There"))
+    lambda_functions()
 
 if __name__ == '__main__':
    main()
