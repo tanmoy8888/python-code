ed264252ed4a31e32043bc2883a0d494fa1edb27
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2026-07-19 13:32:18 +0530

    class and objects

--- python_bootcamp/classes_objects/Car.py
+++ python_bootcamp/classes_objects/Car.py
@@ -1,4 +1,5 @@
 class Car:
+    manufactured_in = "India"
     def __init__(self, model, colour):
         self.model_name= model
         self.colour = colour
@@ -6,9 +7,14 @@ class Car:
     def launch(self):
         print(f"{self.model_name} has {self.colour}")
 
+    @classmethod
+    def show(self):
+        print(f"{self.manufactured_in}")
+
 def main():
     car1 = Car("Maruti", "Blue")
     car1.launch()
+    car1.show()
 
 if __name__ == "__main__":
     main()
