633d97d921b87d537e09daf3254ab7a36c2f32fd
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2026-07-19 13:10:33 +0530

    class and objects

--- python_bootcamp/classes_objects/Car.py
+++ python_bootcamp/classes_objects/Car.py
@@ -1,12 +1,14 @@
 class Car:
-    def _init_(self, model, colour):
+    def __init__(self, model, colour):
         self.model_name= model
         self.colour = colour
+
     def launch(self):
         print(f"{self.model_name} has {self.colour}")
 
-    def main(self):
-        car1 = Car("Maruti","Blue")
-        car1.launch()
-    if __name__ == '__main__':
-        main()
+def main():
+    car1 = Car("Maruti", "Blue")
+    car1.launch()
+
+if __name__ == "__main__":
+    main()
