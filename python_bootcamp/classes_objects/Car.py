6cae5cb7ad2f3f9699a9d199b9f70b480ac18676
Tanmoy Mukherjee <tanmoy8888@yahoo.co.in>
2026-07-19 12:59:29 +0530

    class and objects

---
+++ python_bootcamp/classes_objects/Car.py
@@ -0,0 +1,10 @@
+class Car:
+    def _init_(self, model, colour):
+        self.model_name= model
+        self.colour = colour
+
+    def main():
+        print("")
+        
+    if __name__ == '__main__':
+        main()
